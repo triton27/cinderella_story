2f5ac4bde9c613a5c02c9abbc4ceab6c7b2f494e
toriumi <na54heart@icloud.com>
2019-05-10 03:31:27 +0900

    Add ball

---
+++ ball.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	)
+
+// 舞踏会
+type Ball struct {
+	// Mutex は sync パッケージにある、相互排他ロック
+	//
+	m          sync.Mutex
+	Entries    []*Human
+	Clock	   int // 時刻
+	FinishedAt int
+}
+
+func NewBall(startedAt, finishedAt) *Ball {
+	return &Ball{
+		Clock:      startedAt, // 開始時刻
+		FinishedAt: finishedAt, // 終了時刻
+	}
+}
+
+func (b *Ball) Start() {
+	fmt.Println("舞踏会開始")
+}
+
+func (b *Ball) Dancing() {
+	b.m.Lock()
+	defer b.m.Unlock()
+	fmt.Printf("現在 $d時\n", b.Clock)
+	for _, h := range b.Entries {
+		fmt.Printf("%v は踊っている\n", h.Name)
+	}
+	b.Clock++
+}
+
+func (b *Ball) Finish() {
+	fmt.Println("舞踏会は終了")
+}
+
+func (b *Ball) IsFinished() bool {
+	return b.Clock >= b.FinishedAt
+}
+
+func (b *Ball) Entry(h *Human) {
+	if h.Cos != nil {
+		b.Entries = append(b.Entries, h)
+		fmt.Printf("%v は舞踏会に参加します\n", h.Name)
+	} else {
+		fmt.Println("衣装を持っていないと参加できません")
+		fmt.Printf("%v は舞踏会に参加できない\n", h.Name)
+	}
+}
+
+func (b *Ball) Exit(h *Human) {
+	b.m.Lock()
+	defer b.m.Unlock()
+	var entries []*Human
+	for _, e := range b.Entries {
+		if e != h {
+			entries = append(entries, e)
+		}
+	}
+	b.Entries = entries
+	fmt.Printf("%v は舞踏会から抜け出し、帰宅した。\n", h.Name)
+}

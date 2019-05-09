b1a797781d4fde2cb2f122b75467d5779cf5976f
toriumi <na54heart@icloud.com>
2019-05-10 02:56:54 +0900

    wip cinderella.go

---
+++ cinderella.go
@@ -0,0 +1,9 @@
+stepMother := NewHuman("StepMother", 52, Woman)
+sisterA := NewHuman("SisterA", 23, Woman)
+sisterB := NewHuman("SisterB", 20, Woman)
+cinderella := NewHuman("ella", 18, Woman)
+
+stepMother.Say("ネコに餌をやれ")
+sisterA.Say("洗濯しておいて")
+sisterB.Say("お腹空いた")
+cinderella.Say("...")

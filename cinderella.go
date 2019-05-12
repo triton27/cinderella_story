5edbe5b99d801b11a15683c755231badb330ddaf
toriumi <na54heart@icloud.com>
2019-05-13 00:32:06 +0900

    Add cinderella going to the ball

--- cinderella.go
+++ cinderella.go
@@ -33,3 +33,18 @@ ball.Entry(sisterB)
 // シンデレラのドレスだけない
 dressRoom.GetDress(cinderella)
 ball.Entry(cinderella)
+
+magic := NewMagic(cinderella)
+cinderella.SetCostume(magic.GeneraeDress())
+cinderella.SetShoes(magic.GenerateGlassShoes())
+
+limit := make(chan int, 1)
+go magic.Limit(limit)
+// シンデレラが舞踏会に参加する
+ball.Entry(cinderella)
+
+// 王子登場
+prince := NewHuman("王子", 18, Man)
+tailcoat := NewTailcoat(prince)
+prince.SetCostume(tailcoat)
+ball.Entry(prince)
--- magic.go
+++ magic.go
@@ -10,6 +10,7 @@ type Magic struct {
 func NewMagic(h *Human) *Magic {
 	return &Magic{
 		Target: h,
+		// キャパシティを１に指定する
 		Broken: make(chan int, 1),
 	}
 }
@@ -24,4 +25,15 @@ func (m *Magic) GenerateDress() Costume {
 func (m *Magic) GenerateGlassShoes() *Shoes {
 	fmt.Printf("%v は魔法のガラスの靴を作ってもらった！\n", m.Target.Name)
 	return NewShoes(m.Target)
+}
+
+func (m *Magic) Limit(limit chan int) {
+	// limit から値を受信する
+	// 値を受信するまで、処理をブロックする
+	// limit から値を受信すると、魔法が解けるという筋書きにする
+	<-limit
+	fmt.Println("0時が近づいている")
+	fmt.Println("魔法が解けそう！！")
+	// channel 型の Broken に１を送信
+	m.Broken <- 1
 }
